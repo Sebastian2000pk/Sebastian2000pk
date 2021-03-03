a177f7fdcbbf657e3c5f6a3d18cce2f616b2a5d2
Sebastianpk <juochoas@unal.edu.co>
2021-03-03 12:31:24 -0500

    Se agrego el efecto de hover al menu

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,6 +8,15 @@ import background from '../assets/static/background-home.jpg';
 const Home = ({children}) => (
   <div className="home" style={{ backgroundImage: `url(${background})` }}>
     {children}
+    <div className="home-text">
+      <p className="title-home">Web Developer</p>
+      <p className="home-name">Juan Sebastián Ochoa</p>
+      <p className="home-description">
+        Desarrollador de software colombiano <br/>
+        Estudiante de la Universidad Nacional de Colombia <br/>
+        20 años de edad <br/>
+      </p>
+    </div>
   </div>
 );
 
